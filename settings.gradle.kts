097c7dd7ee1bf80cd1ad141089a13a8015660e67
Leo Kwan <leojkwan@gmail.com>
2024-04-27 17:36:40 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+// TODO replace "My Application" with a better name for the project.
+rootProject.name = "My Application"
+include(":app")
